4ae86ff6cb3e19190416d5c7ab549d14edf00832
Ashwin Sekhar T K <ashwinyes@users.noreply.github.com>
2017-07-07 12:26:44 +0530

    Update clang_compile.sh

--- android/clang_compile.sh
+++ android/clang_compile.sh
@@ -1,7 +1,8 @@
-export AR=ar
-export NDK_ROOT=/ndk
+export NDK_ROOT=/home/ashwin/workspace/android/sdk/ndk-bundle
+export LDFLAGS="-L${NDK_ROOT}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.9.x"
+export FLAGS="-target arm-linux-androideabi -marm -mfpu=vfp -mfloat-abi=softfp --sysroot ${NDK_ROOT}/platforms/android-23/arch-arm"
 
-clang -target arm-linux-androideabi -marm -mfpu=vfp -mfloat-abi=softfp \
---sysroot $(NDK_ROOT)/platforms/android-23/arch-arm \
--L$(NDK_ROOT)/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.9.x \
-$1
+#OpenBLAS Compile
+make TARGET=ARMV7 NO_LAPACK=1 USE_THREAD=0 AR=ar CC="clang ${FLAGS}" HOSTCC=gcc ONLY_CBLAS=1 -j4
+                                                                                           
+
